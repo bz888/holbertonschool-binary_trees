1c4619455b50b0784d5d32b57fda63a17b4abf7a
bz888 <ziyu.ben158@gmail.com>
2026-04-06 13:23:58 +1000

    15

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_depth - find depth of binary tree
+ * @tree: pointer to the node to measure depth
+ * Return: height of a binary tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+
+	return (depth);
+}
+
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_height - find height of binary tree
+ * @tree: pointer to the root node
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right, height;
+
+	if (!tree)
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left > right)
+		height = left;
+	else
+		height = right;
+
+	return (height + 1);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a node
+ * @tree: pointer to the node to measure
+ * Return: balance factor, height of left - right
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance = 0;
+
+	if (!tree)
+		return (0);
+
+	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+
+	return (balance);
+}
+
